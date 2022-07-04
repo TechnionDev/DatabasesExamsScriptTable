76c339da555deb113249f3e4e0d403f939c76961
Gur Telem <gurt@apple.com>
2022-07-04 12:00:49 +0300

    Allow manual skips. Skip C/Mid terms exams with warnings

--- addHyperlinksToExcel.py
+++ addHyperlinksToExcel.py
@@ -2,35 +2,48 @@ import openpyxl
 import os
 import re
 from urllib.request import pathname2url
-filename = "examsLinksTemplate.xlsx"
-prev_exams_dir = "./prevExams"
+TEMPLATE_FILENAME = "examsLinksTemplate.xlsx"
+PREV_EXAMS_DIR = "./prevExams"
+OUTPUT_FILENAME = 'examsLinks.xlsx'
+
+warning_text = ""
 
 
 def path_to_year_semester_moed_is_solution(path):
-    if 'DS_Store' in path or 'idterm' in path:
-        return
-    if "Sp" in path:
-        term = 'Spring'
-    elif 'W' in path:
-        term = 'Winter'
-    else:
-        raise Exception("Term not found")
+    global warning_text
+    # Replace all hebrew letters with coresponding latin
+    path = path.replace('חורף', 'Winter')
+    path = path.replace('אביב', 'Spring')
+    path = path.replace('מועד', 'Moed')
+    path = path.replace('פתרון', 'Solution')
+    path = path.replace('בוחן אמצע', 'Midterm')
+    path = path.replace('בוחן', 'Midterm')
+    path = path.replace('אמצע', 'Midterm')
+    path = path.replace('א', 'A')
+    path = path.replace('ב', 'B')
 
     try:
+        # Ignore the PREV_EXAMS_DIR part of the path
+        path = path[path.find(PREV_EXAMS_DIR)+len(PREV_EXAMS_DIR):]
+        if re.search(r'midterm', path, re.IGNORECASE) is not None or 'skip' in path:
+            # Skip midterms and skips
+            return None
+
+        if 'DS_Store' in path or 'idterm' in path:
+            return
+        if "Sp" in path:
+            term = 'Spring'
+        elif 'W' in path:
+            term = 'Winter'
+        else:
+            raise Exception("Term not found")
+
         # extract year in the form of [0-9]{2,4}
         year = "20"+re.search(r'\d{2,4}', path).group(0)[-2:]
 
         # Extract moed from path (follows the work Moed ignore case)
-        # Replace all hebrew letters with coresponding latin
-        path = path.replace('חורף', 'Winter')
-        path = path.replace('אביב', 'Spring')
-        path = path.replace('מועד', 'Moed')
-        path = path.replace('א', 'A')
-        path = path.replace('ב', 'B')
-        path = path.replace('פתרון', 'Solution')
-
         moed = re.search(
-            r'(Moed.?|/|Exam|\d\d|Maman)([AB])', path, re.IGNORECASE).group(2).upper()
+            r'(Moed.?|/|Exam|\d\d|Maman)([ABC])', path, re.IGNORECASE).group(2).upper()
         # Check if "sol" apear in the path case insensitive
         is_sol = re.search(r'sol', path, re.IGNORECASE) is not None
         print(f'extracted {year=} {term=} {moed=} {is_sol} \t:\t{path=}')
@@ -42,11 +55,12 @@ def path_to_year_semester_moed_is_solution(path):
 
 
 def main():
+    global warning_text
 
-    print(f'Iterating over {prev_exams_dir=}')
+    print(f'Iterating over {PREV_EXAMS_DIR=}')
 
     # Open excel file
-    wb = openpyxl.load_workbook(filename)
+    wb = openpyxl.load_workbook(TEMPLATE_FILENAME)
     sheet = wb.active
 
     # row_offset_from_term_moed = {
@@ -84,16 +98,19 @@ def main():
         last_row = row
 
     # Iterate files over the prev_exams_dir recursively
-    for subdir, dirs, files in os.walk(prev_exams_dir):
+    for subdir, dirs, files in os.walk(PREV_EXAMS_DIR):
         for file in files:
             path = os.path.join(subdir, file)
             ret = path_to_year_semester_moed_is_solution(path)
             if not ret:
+                warning_text += f'Skipped path: {path}\n'
                 continue
             # encode path to url file:// path
             url = f'{pathname2url(path[2:])}'
 
             year, semester, moed, is_sol = ret
+            if (moed == 'C'):
+                warning_text += f'Skipped adding C term exam: {path}\n'
             cell_value = "Moed " + moed + (" Sol" if is_sol else "")
             # cell_value = f'=HYPERLINK("{url}", "'+(moed + " Sol" if is_sol else "") + '")'
             year = int(year)
@@ -110,7 +127,9 @@ def main():
                     # Excel conditional formatting number > 2015
                     conditional_formula = '=IF(AND(ISNUMBER(A{row}),A{row}>2015),"",A{row})'
 
-    wb.save('examsLinks.xlsx')
+    wb.save(OUTPUT_FILENAME)
+    if warning_text:
+        print("WARNING:\n" + warning_text)
 
 
 if __name__ == "__main__":
